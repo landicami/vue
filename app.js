7f6acc5a45865947450a36fea8f391e80f68c969
Camilla Landin <landin.camilla8@gmail.com>
2024-10-03 16:29:11 +0200

    wirking with data and click-events

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  //   template: "<h2>I am the template</h2>",
+  data() {
+    return {
+      title: "The primal empires",
+      author: "Star Wars",
+      age: 44,
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      this.title = title;
+    },
+  },
+});
 
 app.mount("#app");
